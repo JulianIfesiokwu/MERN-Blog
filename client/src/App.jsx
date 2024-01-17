76d4ec0bb5e25c5e5ec44b69e7e54c566975d31a
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-01-17 07:02:54 +0100

    Feat: creating pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,41 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <div className='bg-red-400'>Hello there</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/projects'
+          element={<Projects />}
+        />
+        <Route
+          path='/sign-in'
+          element={<SignIn />}
+        />
+        <Route
+          path='/sign-up'
+          element={<SignUp />}
+        />
+        <Route
+          path='/dashboard'
+          element={<Dashboard />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
