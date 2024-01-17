b2bd1bec414ac4f7f374f3c43e84b493d0672e5d
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-01-17 21:20:57 +0100

    Feat: creating and running server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,12 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+          <Button
+            gradientDuoTone='purpleToBlue'
+            outline
+          >
+            Sign In
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
