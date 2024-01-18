dd87f21a1f72dcccf02b329abb95ffc31b8b4cb3
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-01-18 07:24:14 +0100

    Feat: connecting to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((err) => console.log("Error connecting to DB"));
 
 const app = express();
 
