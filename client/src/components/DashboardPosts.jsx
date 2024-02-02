af40b0db6fbc862fde09f0ae483db50b1cbac684
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-02-02 13:27:28 +0100

    Feat: show user posts in dashboard

--- client/src/components/DashboardPosts.jsx
+++ client/src/components/DashboardPosts.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import { Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const DashboardPosts = () => {
-  return <div>DashboardPosts</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getPosts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+  return (
+    <div className='table-auto overflow-x-auto md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table
+            hoverable
+            className='shadow-md'
+          >
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post, index) => {
+              return (
+                <Table.Body
+                  key={index}
+                  className='divide-y'
+                >
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>
+                      {new Date(post.updatedAt).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link to={`/post/${post.slug}`}>
+                        <img
+                          src={post.image}
+                          alt={post.title}
+                          className='w-20 h-20 object-cover bg-gray-500'
+                        />
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link
+                        className='font-medium text-gray-900 dark:text-white'
+                        to={`/posts/${post.slug}`}
+                      >
+                        {post.title}
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>{post.category}</Table.Cell>
+                    <Table.Cell>
+                      <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                        Delete
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link
+                        className='text-teal-500 hover:underline cursor-pointer'
+                        to={`/update-post/${post._id}`}
+                      >
+                        <span>Edit</span>
+                      </Link>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              );
+            })}
+          </Table>
+        </>
+      ) : (
+        <div className=''>
+          <p>There are currently no posts to display yet.</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPosts;
