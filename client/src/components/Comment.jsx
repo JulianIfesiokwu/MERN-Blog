c2b2dd2cfbb75deef61273c2af4f4aac1dbc5f99
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-02-11 12:05:40 +0100

    Feat: adding delete functionality to comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
   const [user, setUser] = useState({});
   const { currentUser } = useSelector((state) => state.user);
   const [isEditing, setIsEditing] = useState(false);
@@ -120,13 +120,22 @@ const Comment = ({ comment, onLike, onEdit }) => {
               </p>
               {currentUser &&
                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button
-                    className='text-blue-500'
-                    onClick={handleEdit}
-                    type='button'
-                  >
-                    Edit
-                  </button>
+                  <>
+                    <button
+                      className='text-blue-500'
+                      onClick={handleEdit}
+                      type='button'
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className='text-red-500'
+                      onClick={() => onDelete(comment._id)}
+                      type='button'
+                    >
+                      Delete
+                    </button>
+                  </>
                 )}
             </div>
           </>
