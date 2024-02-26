21fcda9fb9451e8eaa7fea7a8c06aea8d0b31173
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-02-26 12:08:26 +0100

    Completed about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className='min-h-screen flex items-center justify-center'>
+        <div className='max-w-2xl mx-auto p-3 text-center'>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            About Julian's Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+              ullam, earum cum laboriosam consequuntur voluptatibus rerum
+              necessitatibus recusandae dolores. Facere atque et, alias
+              voluptates officia provident? Deserunt tempora quos accusantium?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+              ullam, earum cum laboriosam consequuntur voluptatibus rerum
+              necessitatibus recusandae dolores. Facere atque et, alias
+              voluptates officia provident? Deserunt tempora quos accusantium?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+              ullam, earum cum laboriosam consequuntur voluptatibus rerum
+              necessitatibus recusandae dolores. Facere atque et, alias
+              voluptates officia provident? Deserunt tempora quos accusantium?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        excepturi ratione placeat laborum officiis, in, sequi odit eum nostrum
+        quibusdam minima similique incidunt voluptates sed facilis autem neque
+        eligendi nisi.
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
